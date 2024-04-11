682c2c900758d82af5e43e09e35555f2af540fe0
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-04-11 16:59:54 +0200

    Initial Structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -16,4 +15,11 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+sourceSets {
+    test {
+        java {
+            srcDirs("/test")
+        }
+    }
 }
---
+++ src/test/java/com/iesam/digitallibrary/MainTest.java
@@ -0,0 +1,7 @@
+package com.iesam.digitallibrary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
