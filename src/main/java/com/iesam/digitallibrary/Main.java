ac21f5a22f912943843a1d55347cf29e9337ebe9
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-04-15 17:39:23 +0200

    feat: Dar de alta Usuario
    Formulario que Da de alta el usuario creado en el Main
    (Consola de momento).

--- src/main/java/com/iesam/digitallibrary/Main.java
+++ src/main/java/com/iesam/digitallibrary/Main.java
@@ -1,7 +1,26 @@
 package com.iesam.digitallibrary;
 
+import java.util.Scanner;
+import com.iesam.digitallibrary.features.user.domain.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("MENÚ PRINCIPAL");
+        System.out.println("1. Registrar usuario");
+
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Introduce el nombre del usuario");
+                String name = scanner.nextLine();
+                System.out.println("Introduce el email del usuario");
+                String email = scanner.nextLine();
+                System.out.println("Introduce el DNI del usuario");
+                String DNI = scanner.nextLine();
+                User user = new User(name, email, DNI);
+                break;
+        }
+        scanner.close();
     }
 }
